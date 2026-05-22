3ef6aca57cdcf9a6eebd8bcf373c542e32820028
deepanshuvijaysharma <deepanshuvijaysharma@gmail.com>
2026-05-22 11:37:54 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  res.send("CI/CD Pipeline Running");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
